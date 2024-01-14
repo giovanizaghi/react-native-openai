4484ede89fa3d0bcd5ab567f3cd02fdd57f7a5a8
Giovani Zaghi <giovanizaghinogueire@gmail.com>
2024-01-14 16:28:13 -0300

    adição de testes

--- src/tests/openai.test.ts
+++ src/tests/openai.test.ts
@@ -3,46 +3,26 @@ import { eModel } from "../enums/eModel";
 import { eResponseFormat } from "../enums/eResponseFormat";
 import { eSize } from "../enums/eSize";
 import { eStyle } from "../enums/eStyle";
-import HttpInterceptor from "../httpInterceptor";
 import { OpenAI } from "../openai/OpenAI";
 
 
-jest.mock('../httpInterceptor', () => {
-    return {
-        __esModule: true,
-        default: jest.fn().mockImplementation(() => ({
-            getInstance: jest.fn(),
-        })),
-    };
-});
-
-// Mocking axios methods
-jest.mock('axios', () => ({
-    create: jest.fn(),
-}));
-
 describe('OpenAI', () => {
-    const mockApiKey = 'mock-api-key';
-    const mockOrganization = 'mock-organization';
+    let openAI: OpenAI;
+    const mockApiKey = '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';
+    const mockOrganization = 'org-xDgkNCcBtmEluQvm2CxofvV1';
 
-    it('should create OpenAI instance', () => {
-        const openAI = new OpenAI(mockApiKey, mockOrganization);
-        const mockHttpInterceptor = new HttpInterceptor('mock-api-key', 'mock-organization');
-
-        openAI.setBaseURLForTesting(mockHttpInterceptor);
-
-        expect(openAI['baseURL']).toBeDefined();
-    });
+    beforeEach(() => {
+        openAI = new OpenAI(mockApiKey, mockOrganization);
+      });
+    
 
     it('should create Image with mocked HTTP call (createImage)', async () => {
-        const openAI = new OpenAI(mockApiKey, mockOrganization);
-        const mockImageResponse = { created: Date.now(), data: [{ url: 'mock-image-url' }] };
+        const mockedAxios = jest.spyOn(openAI.images['baseURL'], 'post');
 
-        jest.spyOn(openAI.images, 'createImage').mockResolvedValue(mockImageResponse);
+        const response = await openAI.images.createImage('Foto de um gato spynx', eModel.DallE2, 1, eQuality.standard, eResponseFormat.url, eSize._1024x1024, eStyle.vivid, 'mock-user');
+        console.log("teste", response);
+        expect(mockedAxios).toHaveBeenCalledTimes(1);
+        expect(response).toBeDefined();
+    }, 120000);
 
-        const response = await openAI.images.createImage('mock-prompt', eModel.DallE2, 1, eQuality.standard, eResponseFormat.url, eSize._1024x1024, eStyle.vivid, 'mock-user');
-
-        expect(response).toEqual(mockImageResponse);
-    });
-    
 });
